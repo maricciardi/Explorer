a83fe8d84e80b2449bb626c340d5d9105904076f
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-10-07 18:32:49 -0300

    create desafio final

---
+++ Stage 04 - Introdução aos conceitos do JavaScript/script.js
@@ -0,0 +1,14 @@
+//let name = "Mariana";
+//let age = 23;
+//let stars = 1.5;
+//let isSubscribed = true;
+
+let student = {
+    name: "Mariana",
+    age: 23,
+    stars: 1.5,
+    isSubscribed: true
+};
+
+//console.log(student)
+
