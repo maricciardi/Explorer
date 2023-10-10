4a587324f80891705e1b70c50e3199c97fb37f5c
Mariana Ricciardi <mariana_ricciardi@hotmail.com>
2023-10-09 22:22:25 -0300

    create stage04

---
+++ Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/script.js
@@ -0,0 +1,7 @@
+nome = prompt('Qual o seu nome?')
+alert(`Olá ${nome}! Bora somar dois números?`)
+n1 = Number(prompt('Digite o primeiro número: '))
+n2 = Number(prompt('Digite o segundo número: '))
+resultado = n1 + n2;
+alert(`A soma dos dois números é: ` + resultado);
+
